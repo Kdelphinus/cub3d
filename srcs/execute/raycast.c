a8639397cdf53c56adef478862ec09d354fa4a4c
Danbi Park <danpark@c1r6s6.42seoul.kr>
2023-05-17 19:54:01 +0900

    hahaha the end

--- srcs/execute/raycast.c
+++ srcs/execute/raycast.c
@@ -1,24 +1,5 @@
 #include "cub3d.h"
 
-void	window_init(t_map_data *map_data, t_img *img)
-{
-	int	i;
-	int	j;
-
-	i = -1;
-	while (++i < SCREENHEIGHT)
-	{
-		j = -1;
-		while (++j < SCREENWIDTH)
-		{
-			if (i < SCREENHEIGHT / 2)
-				img->data[i * SCREENWIDTH + j] = map_data->ceil_color;
-			else
-				img->data[i * SCREENWIDTH + j] = map_data->flr_color;
-		}
-	}
-}
-
 void	ray_dir_init(int x, t_ray *ray)
 {
 	ray->camera_x = 2 * x / (double)SCREENWIDTH - 1;
@@ -40,7 +21,8 @@ void	ray_step_init(t_ray *ray)
 	else
 	{
 		ray->step[X] = 1;
-		ray->side_dist[X] = (ray->map[X] + 1.0 - ray->pos[X]) * ray->delta_dist[X];
+		ray->side_dist[X] = (ray->map[X] + 1.0 - ray->pos[X]) * \
+		ray->delta_dist[X];
 	}
 	if (ray->ray_dir[Y] < 0)
 	{
@@ -50,7 +32,8 @@ void	ray_step_init(t_ray *ray)
 	else
 	{
 		ray->step[Y] = 1;
-		ray->side_dist[Y] = (ray->map[Y] + 1.0 - ray->pos[Y]) * ray->delta_dist[Y];
+		ray->side_dist[Y] = (ray->map[Y] + 1.0 - ray->pos[Y]) * \
+		ray->delta_dist[Y];
 	}
 }
 
@@ -74,68 +57,17 @@ void	dda(t_ray *ray, t_map_data *map_data)
 			break ;
 	}
 	if (ray->side == 0)
-		ray->perp_wall_dist = (ray->map[X] - ray->pos[X] + (1 - ray->step[X]) / 2) / ray->ray_dir[X];
+		ray->perp_wall_dist = (ray->map[X] - ray->pos[X] + \
+		(1 - ray->step[X]) / 2) / ray->ray_dir[X];
 	else
-		ray->perp_wall_dist = (ray->map[Y] - ray->pos[Y] + (1 - ray->step[Y]) / 2) / ray->ray_dir[Y];
-}
-
-void	choice_tex(t_ray *ray)
-{
-	if (ray->side == 0 && ray->ray_dir[X] < 0)
-		ray->tex_num = WE;
-	else if (ray->side == 0 && ray->ray_dir[X] > 0)
-		ray->tex_num = EA;
-	else if (ray->side == 1 && ray->ray_dir[Y] < 0)
-		ray->tex_num = NO;
-	else if (ray->side == 1 && ray->ray_dir[Y] > 0)
-		ray->tex_num = SO;
-	if (ray->side == 0)
-		ray->wall_x = ray->pos[Y] + ray->perp_wall_dist * ray->ray_dir[Y];
-	else
-		ray->wall_x = ray->pos[X] + ray->perp_wall_dist * ray->ray_dir[X];
-	ray->wall_x -= floor(ray->wall_x);
-	ray->tex[X] = (int)(ray->wall_x * (double)IMG_W);
-	if (ray->side == 0 && ray->ray_dir[X] < 0)
-		ray->tex[X] = IMG_W - ray->tex[X] - 1;
-	if (ray->side == 1 && ray->ray_dir[Y] > 0)
-		ray->tex[X] = IMG_W - ray->tex[X] - 1;
-}
-
-void	put_color(int x, t_ray *ray, t_texture *texture, t_game_info *game_info)
-{
-	int	j;
-	int	*tex;
-	int	bpp;
-	int	sl;
-	int	endian;
-	int	color;
-
-	ray->line_h = (int)(SCREENHEIGHT / ray->perp_wall_dist);
-	ray->draw_start = -ray->line_h / 2 + SCREENHEIGHT / 2;
-	if (ray->draw_start < 0)
-		ray->draw_start = 0;
-	ray->draw_end = ray->line_h / 2 + SCREENHEIGHT / 2;
-	if (ray->draw_end >= SCREENHEIGHT)
-		ray->draw_end = SCREENHEIGHT - 1;
-	ray->step_d = 1.0 * IMG_H / ray->line_h;
-	ray->tex_pos = (ray->draw_start - SCREENHEIGHT / 2 + ray->line_h / 2) * ray->step_d;
-	tex = (int *)mlx_get_data_addr(texture[ray->tex_num].img, &bpp, &sl, &endian);
-	j = ray->draw_start - 1;
-	while (++j < ray->draw_end)
-	{
-		ray->tex[Y] = (int)ray->tex_pos & (IMG_H - 1);
-		ray->tex_pos += ray->step_d;
-		color = tex[ray->tex[Y] * IMG_W + ray->tex[X]];
-//		if (ray->side == 1)
-//			color /= 2;
-		game_info->img->data[j * SCREENWIDTH + x] = color;
-	}
+		ray->perp_wall_dist = (ray->map[Y] - ray->pos[Y] + \
+		(1 - ray->step[Y]) / 2) / ray->ray_dir[Y];
 }
 
 void	ray_cast(t_ray *ray, t_game_info *game_info)
 {
 	int	x;
-	
+
 	x = -1;
 	window_init(game_info->map_data, game_info->img);
 	while (++x < SCREENWIDTH)
