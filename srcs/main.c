73df15f09d31dd65bdcef06cc5478b108f5a811f
Kdelphinus <kmj951015@gmail.com>
2023-05-18 21:18:27 +0900

    [main.c] 쓸모없는 raycasting 함수 제거

--- srcs/main.c
+++ srcs/main.c
@@ -22,7 +22,6 @@ int	main(int ac, char **av)
 		print_err_exit(WRONGARG);
 	game_info = (t_game_info *)malloc(sizeof(t_game_info));
 	parsing(game_info, av[1]);
-	ray_cast(game_info->ray, game_info);
 	mlx_hook(game_info->win, X_EVENT_KEY_PRESS, 0, &key_press, game_info);
 	mlx_hook(game_info->win, X_EVENT_KEY_EXIT, 0, &ft_close, game_info);
 	mlx_loop_hook(game_info->mlx, &main_loop, game_info);
